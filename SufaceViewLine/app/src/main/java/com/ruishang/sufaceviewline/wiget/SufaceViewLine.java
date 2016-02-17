5516b06711283b9baed524bd923d15f28b1c935a
flowerdo@126.com <flowerdo@126.com>
2016-02-17 16:54:29 +0800

    两条线同时画

--- SufaceViewLine/app/src/main/java/com/ruishang/sufaceviewline/wiget/SufaceViewLine.java
+++ SufaceViewLine/app/src/main/java/com/ruishang/sufaceviewline/wiget/SufaceViewLine.java
@@ -1,53 +1,72 @@
 package com.ruishang.sufaceviewline.wiget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
-import android.widget.Toast;
+
+import com.ruishang.sufaceviewline.R;
 
 /**
+ * 动态线条
  * Created by Jone on 2016/2/16.
  */
-public class SufaceViewLine extends SurfaceView implements SurfaceHolder.Callback, Runnable{
-    private Context mContext;
-    private int width;
-    private int height;
-    private int l;
-    private int n;
-    private int p=60;
-    private int o=5;
+public class SufaceViewLine extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    private static final int UP = 0; //初始位置在上方
+    private static final int DONW = 1; //初始位置在下方
+    private static final int ACT = 0; //循环
+    private static final int ONCE = 1; //一次
+    private static final String TAG = SufaceViewLine.class.getSimpleName();
+
+    private Context mContext;//上下文
+
+    private int width; //屏幕宽度
+    private int height; //屏幕高度
+    private int l; //分段线长度
+    private int p = 35;//Y轴起伏高度
+    private int speed = 3;//步进速度
+    private int state = ACT; //线条是否动态
+    private int direction = UP; //线条初始方向
+    private boolean stop = false;//停止线程标志
+    private int defaultColor = Color.GREEN;
+
 
     public SufaceViewLine(Context c) {
         super(c);
         init(c);
     }
 
-    public void init(Context c){
-        this.mContext=c;
+    public void init(Context c) {
+        this.mContext = c;
         this.surfaceHolder = this.getHolder();
         this.surfaceHolder.addCallback(this);
+        this.setZOrderOnTop(true);
+        this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
         this.obj = new GameObject();
         WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
         this.width = wm.getDefaultDisplay().getWidth();
         this.height = wm.getDefaultDisplay().getHeight();
-        jisuan(100);
-        this.l = (this.width-2*n)/3;
+        l=(width - 2 * p) / 3;
     }
 
     public SufaceViewLine(Context c, AttributeSet attrs) {
-        super(c, attrs);
-        init(c);
+        this(c, attrs, 0);
     }
 
     public SufaceViewLine(Context c, AttributeSet attrs, int defStyleAttr) {
         super(c, attrs, defStyleAttr);
+        TypedArray ta = c.obtainStyledAttributes(attrs, R.styleable.SufaceViewLine);
+        this.state = ta.getInt(R.styleable.SufaceViewLine_lineType, ACT);
+        this.defaultColor = ta.getColor(R.styleable.SufaceViewLine_lineColor, Color.WHITE);
+        ta.recycle();
         init(c);
     }
 
@@ -57,97 +76,147 @@ public class SufaceViewLine extends SurfaceView implements SurfaceHolder.Callbac
     class GameObject {
         private float x;
         private float y;
+        private float y2;
         private Paint paint;
-        private boolean redraw=false;
-        private float pts[]={
-                0,0,0,0,
-                0,0,0,0,
-                0,0,0,0,
-                0,0,0,0,
-                0,0,0,0
+        private boolean redraw = false;
+        private float pts[] = {
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
         };
 
         public GameObject() {
             this.x = 0;
-            this.y = 0;
-
-
+            this.y = 5;
+            this.y2 = p;
+            if (direction == DONW) {
+                y = p;
+            }
             this.paint = new Paint();
-            this.paint.setColor(colors[0]);
-            this.paint.setStrokeWidth(10);
+            this.paint.setColor(defaultColor);
+            this.paint.setStrokeWidth(3);
             paint.setAntiAlias(true);
+            paint.setStrokeCap(Paint.Cap.ROUND);
         }
 
         // 在SurfaceView加锁同步后传给自己的Canvas上绘制自己
         public void drawSelf(Canvas canvas) {
             //canvas.drawLine(x, y, x2, y2,paint);
-            canvas.drawLines(pts,paint);
+            canvas.drawLines(pts, paint);
         }
 
-        // 获取物件下一次要绘制的位置(这里是沿着一个边长为400的正方形不断运动的)
-        public void getNextPos() {
-            if(y==0&&x<l){
-                x+=o;
-                pts[2]=x;
-            }else if(y<p&&x<l+n){
-                x+=o;
-                y+=o;
-                if(pts[4]==0){
-                    pts[4]=x-o;
-                    pts[5]=y-o;
+        /**
+         * 获取物件下一次要绘制的位置
+         */
+        public void getNextPosUP() {
+            if (y == 5 && x < l) {
+                x += speed;
+                if (pts[1] == 0) {
+                    pts[0] = x - speed;
+                    pts[1] = y;
+                    pts[20] = x - speed;
+                    pts[21] = y2;
                 }
-                pts[6]=x;
-                pts[7]=y;
-            }else if(y==p&&x<2*l+n){
-                x+=o;
-                if(pts[8]==0){
-                    pts[8]=x-o;
-                    pts[9]=y;
+                pts[2] = x;
+                pts[3] = y;
+                pts[22] = x;
+                pts[23] = y2;
+
+            } else if (y < p && x < l + p-5) {
+                x += speed;
+                y += speed;
+                y2 -= speed;
+                if (pts[4] == 0) {
+                    pts[4] = x - speed;
+                    pts[5] = y - speed;
+                    pts[24] = x - speed;
+                    pts[25] = y2 + speed;
                 }
-                pts[10]=x;
-                pts[11]=y;
-            }else if(y>0&&x<2*l+2*n){
-                x+=o;
-                y-=o;
-                if(pts[12]==0){
-                    pts[12]=x-o;
-                    pts[13]=y+o;
+                pts[6] = x;
+                pts[7] = y;
+                pts[26] = x;
+                pts[27] = y2;
+            } else if (y == p && x < 2 * l + p-5) {
+                x += speed;
+                if (pts[8] == 0) {
+                    pts[8] = x - speed;
+                    pts[9] = y;
+                    pts[28] = x - speed;
+                    pts[29] = y2;
                 }
-                pts[14]=x;
-                pts[15]=y;
-            }else if(y==0&&x<width){
-                x+=o;
-                if(pts[18]==0){
-                    pts[17]=y;
-                    pts[16]=x-o;
+                pts[10] = x;
+                pts[11] = y;
+                pts[30] = x;
+                pts[31] = y2;
+            } else if (y > 5 && x < 2 * l + 2 * p-10) {
+                x += speed;
+                y -= speed;
+                y2 += speed;
+                if (pts[12] == 0) {
+                    pts[12] = x - speed;
+                    pts[13] = y + speed;
+                    pts[32] = x - speed;
+                    pts[33] = y2 - speed;
                 }
-                pts[18]=x;
-                pts[19]=y;
-            }else if(x>width){
-                x=0;
-                pts=new float[]{
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0
+                pts[14] = x;
+                pts[15] = y;
+                pts[34] = x;
+                pts[35] = y2;
+            } else if (y == 5 && x < width) {
+                x += speed;
+                if (pts[18] == 0) {
+                    pts[17] = y;
+                    pts[16] = x - speed;
+                    pts[37] = y2;
+                    pts[36] = x - speed;
+                }
+                pts[18] = x;
+                pts[19] = y;
+                pts[38] = x;
+                pts[39] = y2;
+            } else if (x >= width) {
+                if (state == ONCE) {
+                    stop = true;
+                }
+                x = 0;
+                y = 5;
+                y2 = p;
+                pts = null;
+                pts = new float[]{
+                        0, y, 0, y,
+                        0, 0, 0, 0,
+                        0, 0, 0, 0,
+                        0, 0, 0, 0,
+                        0, 0, 0, 0,
+                        0, p, 0, p,
+                        0, 0, 0, 0,
+                        0, 0, 0, 0,
+                        0, 0, 0, 0,
+                        0, 0, 0, 0,
                 };
                 i++;
-                if(i>=colors.length){
-                    i=0;
+                if (i >= colors.length) {
+                    i = 0;
                 }
-                this.paint.setColor(colors[i]);
+                // this.paint.setColor(colors[i]);
             }
 
         }
 
-        public boolean getRedraw(){
-            return redraw;
+        public float getX() {
+            return x;
         }
     }
 
-    int i=0;
-    private int colors[] = {Color.WHITE,Color.BLUE,Color.YELLOW,Color.RED};
+    int i = 0;
+    private int colors[] = {Color.GREEN, Color.BLUE, Color.YELLOW, Color.RED};
     private Thread thread; // SurfaceView通常需要自己单独的线程来播放动画
     private Canvas canvas;
     private SurfaceHolder surfaceHolder;
@@ -155,20 +224,23 @@ public class SufaceViewLine extends SurfaceView implements SurfaceHolder.Callbac
     private GameObject obj;
 
 
-
     @Override
     public void run() {
         while (true) {
-            obj.getNextPos();
-            canvas = this.surfaceHolder.lockCanvas(); // 通过lockCanvas加锁并得到該SurfaceView的画布
-            if(obj.getRedraw()){
-                //canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            obj.getNextPosUP();
+            if (stop) {
+                return;
             }
-            canvas.drawColor(Color.BLACK);
+            canvas = this.surfaceHolder.lockCanvas(); // 通过lockCanvas加锁并得到該SurfaceView的画布
+            if (state == ACT)
+                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
             obj.drawSelf(canvas); // 把SurfaceView的画布传给物件，物件会用这个画布将自己绘制到上面的某个位置
             this.surfaceHolder.unlockCanvasAndPost(canvas); // 释放锁并提交画布进行重绘
+            if (obj.getX() == width) {
+                //return;
+            }
             try {
-                Thread.sleep(1); // 这个就相当于帧频了，数值越小画面就越流畅
+                Thread.sleep(8); // 这个就相当于帧频了，数值越小画面就越流畅
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -178,12 +250,12 @@ public class SufaceViewLine extends SurfaceView implements SurfaceHolder.Callbac
 
     @Override
     public void surfaceDestroyed(SurfaceHolder arg0) {
-        Toast.makeText(mContext, "SurfaceView已经销毁", Toast.LENGTH_LONG).show();
+        // Toast.makeText(mContext, "SurfaceView已经销毁", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder arg0) {
-        Toast.makeText(mContext, "SurfaceView已经创建n:"+n+"L:"+l, Toast.LENGTH_LONG).show();
+        // Toast.makeText(mContext, "SurfaceView已经创建n:"+n+"L:"+l, Toast.LENGTH_LONG).show();
         this.thread = new Thread(this);
         this.thread.start();
     }
@@ -191,18 +263,6 @@ public class SufaceViewLine extends SurfaceView implements SurfaceHolder.Callbac
     @Override
     public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
         // 这里是SurfaceView发生变化的时候触发的部分
-    }
-
-
-    //计算间距
-    public void jisuan(int i){
-        if(((width-2*i)/3+i)%5==0){
-            n=i;
-            o=5;
-        }else{
-            if(i>=30)
-                jisuan(i-1);
-        }
     }
 
 }
